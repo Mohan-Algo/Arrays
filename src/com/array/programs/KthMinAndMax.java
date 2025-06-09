61079fa17297b3cfcafe6ea96081dab4aee1d3cc
Mohan-Algo <mail2mohan7867@gmail.com>
2025-06-10 00:46:44 +0530

    KthMinMax

---
+++ src/com/array/programs/KthMinAndMax.java
@@ -0,0 +1,32 @@
+package com.array.programs;
+
+public class KthMinAndMax {
+
+	public static void main(String[] args) {
+		//int arr[] = {7, 10, 4, 3, 20, 15};
+		int arr[]= {2, 3, 1, 20, 15};
+		int k = 4;
+		
+		sortArray(arr);
+		
+		System.out.println(":::::::::::Kth Min element:::::::::"+arr[k-1]);
+		System.out.println(":::::::::::Kth Max element:::::::::"+arr[(arr.length-1)-(k-1)]);
+		
+
+	}
+
+	private static void sortArray(int[] arr) {
+		int arrLength= arr.length;
+		for(int i=0; i< arrLength-1; i++) {
+			for(int j=i+1; j<arrLength; j++) {
+				if(arr[i]> arr[j]) {
+					int temp= arr[i];
+					arr[i]= arr[j];
+					arr[j]= temp;
+				}
+			}
+		}
+		
+	}
+
+}
